fa3f5f4e55df194c1f6e5e155832d04d995c3f6b
VarunGhagre <varunghagre@gmail.com>
2026-05-05 19:26:58 +0530

    Update file

--- stayfinder-backend/src/modules/user/user.route.js
+++ stayfinder-backend/src/modules/user/user.route.js
@@ -1,11 +1,13 @@
 import express from "express";
 import { registerUser, loginUser } from "./user.controller.js";
 import { protect } from "../../middleware/authMiddleware.js";
+import { updateUserProfile } from "./user.controller.js";
 
 const router = express.Router();
 
 router.post("/register", registerUser);
 router.post("/login", loginUser);
+router.put("/profile", protect, updateUserProfile);
 
 router.get("/profile", protect, (req, res) => {
   res.json(req.user);
