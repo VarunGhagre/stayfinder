35edf9224ae6c303317c7836e0a39689a4b1e8bb
VarunGhagre <varunghagre@gmail.com>
2026-05-13 23:54:55 +0530

    Updata file

--- stayfinder-backend/src/modules/review/review.model.js
+++ stayfinder-backend/src/modules/review/review.model.js
@@ -1,27 +1,33 @@
 import mongoose from "mongoose";
 
-const reviewSchema = new mongoose.Schema({
+const reviewSchema = new mongoose.Schema(
+  {
+    user: {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: "User",
+    },
 
-  user: {
-    type: mongoose.Schema.Types.ObjectId,
-    ref: "User"
+    room: {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: "Room",
+    },
+
+    rating: {
+      type: Number,
+      required: true,
+      min: 1,
+      max: 5,
+    },
+
+    comment: {
+      type: String,
+      required: true,
+    },
   },
+  { timestamps: true }
+);
 
-  room: {
-    type: mongoose.Schema.Types.ObjectId,
-    ref: "Room"
-  },
-
-  rating: {
-    type: Number,
-    min: 1,
-    max: 5
-  },
-
-  comment: {
-    type: String
-  }
-
-}, { timestamps: true });
-
-export default mongoose.model("Review", reviewSchema);
+export default mongoose.model(
+  "Review",
+  reviewSchema
+);
