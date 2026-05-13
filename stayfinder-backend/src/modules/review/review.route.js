4333ebd157fae007e694da6164c6c8c186fec798
VarunGhagre <varunghagre@gmail.com>
2026-05-13 23:58:44 +0530

    Updata file

--- stayfinder-backend/src/modules/review/review.route.js
+++ stayfinder-backend/src/modules/review/review.route.js
@@ -1,9 +1,12 @@
 import express from "express";
 import { addReview } from "./review.controller.js";
 import { protect } from "../../middleware/authMiddleware.js";
+import { getRoomReviews } from "./review.controller.js";
 
 const router = express.Router();
 
 router.post("/:roomId", protect, addReview);
+
+router.get("/:roomId", getRoomReviews);
 
 export default router;
