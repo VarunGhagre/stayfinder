f3a5339138e980f6f696d4fee0d6a772d3115b27
VarunGhagre <varunghagre@gmail.com>
2026-05-14 20:44:41 +0530

    Update code add new function

--- stayfinder-frontend/src/components/Layout.jsx
+++ stayfinder-frontend/src/components/Layout.jsx
@@ -1,10 +1,12 @@
 import Navbar from "./Navbar";
+import Footer from "./Footer";
 
 function Layout({ children }) {
   return (
     <div className="bg-[#0E0E0F] min-h-screen text-[#F2EDE6]">
       <Navbar />
       {children}
+      <Footer />
     </div>
   );
 }
