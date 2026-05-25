51d4a7d41a45d7e205d5985dbb34f9179acabc14
VarunGhagre <varunghagre@gmail.com>
2026-05-25 14:14:16 +0530

    Update Code

--- stayfinder-backend/src/server.js
+++ stayfinder-backend/src/server.js
@@ -7,4 +7,14 @@ connectDB();
 
 const PORT = process.env.PORT || 5000;
 
-app.listen(PORT, () => console.log(`Server running on ${PORT}`));
+// ✅ Only run locally
+if (process.env.NODE_ENV !== "production") {
+
+  app.listen(PORT, () =>
+    console.log(`Server running on ${PORT}`)
+  );
+
+}
+
+// ✅ Export for Vercel
+export default app;
