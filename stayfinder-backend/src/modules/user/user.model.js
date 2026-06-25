bae6fef9806477b874e9cf3fdbeb31071a354230
VarunGhagre <varunghagre@gmail.com>
2026-06-25 23:43:49 +0530

    change's code

--- stayfinder-backend/src/modules/user/user.model.js
+++ stayfinder-backend/src/modules/user/user.model.js
@@ -2,25 +2,91 @@ import mongoose from "mongoose";
 
 const userSchema = new mongoose.Schema({
   name: String,
-  email: { type: String, unique: true },
+
+  email: {
+    type: String,
+    unique: true,
+  },
+
   password: String,
+
   role: {
     type: String,
     enum: ["user", "owner", "admin"],
     default: "user",
   },
 
-   mobile: {
+  mobile: {
     type: String,
     required: true,
   },
 
   country: {
     type: String,
-    enum: ["India", "USA", "UK", "Canada", "Australia"], // 👈 options
+    enum: [
+      "India",
+      "USA",
+      "UK",
+      "Canada",
+      "Australia",
+    ],
     required: true,
   },
-  
+
+  // ======================
+  // OWNER DETAILS
+  // ======================
+
+  propertyName: {
+    type: String,
+  },
+
+  propertyType: {
+    type: String,
+    enum: [
+      "Hotel",
+      "Resort",
+      "PG",
+      "Hostel",
+      "Apartment",
+    ],
+  },
+
+  businessAddress: {
+    type: String,
+  },
+
+  licenseNumber: {
+    type: String,
+  },
+
+  ownerIdNumber: {
+    type: String,
+  },
+
+  ownerStatus: {
+  type: String,
+  enum: [
+    "pending",
+    "approved",
+    "rejected"
+  ],
+  default: "pending"
+},
+
+isEmailVerified: {
+  type: Boolean,
+  default: false
+},
+
+isMobileVerified: {
+  type: Boolean,
+  default: false
+},
+
 });
 
-export default mongoose.model("User", userSchema);
+export default mongoose.model(
+  "User",
+  userSchema
+);
